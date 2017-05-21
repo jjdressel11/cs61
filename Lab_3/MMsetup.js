9cc9625d100277ac037c74ec1607645dbf7d6a5e
Max <max@sighten.io>
2017-05-21 17:51:26 -0400

    added reviewers, manuscripts

--- Lab_3/MMsetup.js
+++ Lab_3/MMsetup.js
@@ -21,7 +21,7 @@ db.createCollection("manuscript_review");
 // set up counter for increasing RI_Code id
 db.counters.insert(
    {
-      "_id": "RI_Code",
+      _id : "RI_Code",
       "seq": 0
    }
 );
@@ -29,7 +29,7 @@ db.counters.insert(
 // counter for increasing editor id
 db.counters.insert(
    {
-      "_id": "editor_ID",
+      _id : "editor_ID",
       "seq": 0
    }
 );
@@ -37,7 +37,7 @@ db.counters.insert(
 // counter for increasing reviewer id
 db.counters.insert(
    {
-      "_id": "reviewer_ID",
+      _id : "reviewer_ID",
       "seq": 0
    }
 );
@@ -45,14 +45,14 @@ db.counters.insert(
 // counter for increasing author id
 db.counters.insert(
    {
-      "_id": "author_ID",
+      _id : "author_ID",
       "seq": 0
    }
 );
 
 db.counters.insert(
    {
-      "_id": "manuscript_ID",
+      _id : "manuscript_ID",
       "seq": 0
    }
 );
@@ -570,3 +570,367 @@ db.author.insertMany(
       affiliation : "Mauris Industries"
     }
   ]);
+
+db.reviewer.insertMany([
+    {
+        _id : getNextSequence("editor_ID"),
+        first_name : "Evelyn",
+        last_name : "Adams",
+        email: "massa.non@ligula.edu",
+        affiliation: "Eu Arcu Morbi Associates"
+    },
+    {
+        _id : getNextSequence("editor_ID"),
+        first_name : "Tasha",
+        last_name : "Clarke",
+        email: "isus.varius@consectetueradipiscingelit.org",
+        affiliation: "Risus Quis Associates"
+    },
+    {
+        _id : getNextSequence("editor_ID"),
+        first_name : "Davis",
+        last_name : "Marks",
+        email: "sed.facilisis@Maecenasmalesuadafringilla.org",
+        affiliation: "Mus LLC"
+    },
+    {
+        _id : getNextSequence("editor_ID"),
+        first_name : "Yuli",
+        last_name : "Gordon",
+        email: "nisi@Nullamvelitdui.ca",
+        affiliation: "Malesuada PC"
+    },
+    {
+        _id : getNextSequence("editor_ID"),
+        first_name : "Cameron",
+        last_name : "Vazquez",
+        email: "vel@montesnascetur.edu",
+        affiliation: "Nunc Company"
+    },
+    {
+        _id : getNextSequence("editor_ID"),
+        first_name : "Victoria",
+        last_name : "Acevedo",
+        email: "eu.lacus.Quisque@aliquetdiam.org",
+        affiliation: "At Pede Incorporated"
+    },
+    {
+        _id : getNextSequence("editor_ID"),
+        first_name : "Brenna",
+        last_name : "Morrow",
+        email: "risus.In.mi@miAliquamgravida.net",
+        affiliation: "Ultrices Posuere Foundation"
+    },
+    {
+        _id : getNextSequence("editor_ID"),
+        first_name : "Kato",
+        last_name : "Serrano",
+        email: "id.enim@Sedeueros.ca",
+        affiliation: "Sed Industries"
+    },
+    {
+        _id : getNextSequence("editor_ID"),
+        first_name : "Judah",
+        last_name : "Conway",
+        email: "massa.rutrum.magna@Utnec.ca",
+        affiliation: "Dolor Tempus Non LLC"
+    },
+    {
+        _id : getNextSequence("editor_ID"),
+        first_name : "Gretchen",
+        last_name : "George",
+        email: "porttitor.scelerisque@adipiscing.edu",
+        affiliation: "Lobortis Nisi PC"
+    }
+]);
+
+db.manuscript.insertMany([
+    {
+        _id : getNextSequence("manuscript_ID"),
+        "editor_ID": 7,
+        author_ID : 4,
+        title : "sed tortor.",
+        status : "accepted",
+        status_time_stamp : "Wed Jun 14th 2017 11:06:41",
+        RI_Code : 89
+    },
+    {
+        _id : getNextSequence("manuscript_ID"),
+        "editor_ID": 10,
+        author_ID : 3,
+        title : "gravida nunc",
+        status : "scheduled for publication",
+        status_time_stamp : "Thu Dec 29th 2016 07:12:29",
+        RI_Code : 84
+    },
+    {
+        _id : getNextSequence("manuscript_ID"),
+        "editor_ID": 9,
+        author_ID : 10,
+        title : "id",
+        status : "accepted",
+        status_time_stamp : "Sat Aug 6th 2016 04:08:20",
+        RI_Code : 101
+    },
+    {
+        _id : getNextSequence("manuscript_ID"),
+        "editor_ID": 2,
+        author_ID : 3,
+        title : "nulla. Integer",
+        status : "scheduled for publication",
+        status_time_stamp : "Fri Dec 8th 2017 02:12:10",
+        RI_Code : 82
+    },
+    {
+        _id : getNextSequence("manuscript_ID"),
+        "editor_ID": 8,
+        author_ID : 6,
+        title : "Proin vel",
+        status : "published",
+        status_time_stamp : "Sat Jun 4th 2016 04:06:32",
+        RI_Code : 42
+    },
+    {
+        _id : getNextSequence("manuscript_ID"),
+        "editor_ID": 6,
+        author_ID : 1,
+        title : "sagittis",
+        status : "published",
+        status_time_stamp : "Sun Apr 1st 2018 03:04:42",
+        RI_Code : 11
+    },
+    {
+        _id : getNextSequence("manuscript_ID"),
+        "editor_ID": 1,
+        author_ID : 9,
+        title : "hendrerit neque. In",
+        status : "scheduled for publication",
+        status_time_stamp : "Fri Mar 10th 2017 11:03:03",
+        RI_Code : 117
+    },
+    {
+        _id : getNextSequence("manuscript_ID"),
+        "editor_ID": 4,
+        author_ID : 4,
+        title : "aliquet, sem ut",
+        status : "under review",
+        status_time_stamp : "Fri Dec 9th 2016 02:12:14",
+        RI_Code : 73
+    },
+    {
+        _id : getNextSequence("manuscript_ID"),
+        "editor_ID": 7,
+        author_ID : 5,
+        title : "Lorem ipsum dolor",
+        status : "submitted",
+        status_time_stamp : "Wed Jun 21st 2017 09:06:28",
+        RI_Code : 79
+    },
+    {
+        _id : getNextSequence("manuscript_ID"),
+        "editor_ID": 4,
+        author_ID : 8,
+        title : "dis parturient",
+        status : "submitted",
+        status_time_stamp : "Wed May 9th 2018 07:05:03",
+        RI_Code : 46
+    },
+    {
+        _id : getNextSequence("manuscript_ID"),
+        "editor_ID": 7,
+        author_ID : 6,
+        title : "euismod et,",
+        status : "in typesetting",
+        status_time_stamp : "Fri Oct 14th 2016 09:10:51",
+        RI_Code : 123
+    },
+    {
+        _id : getNextSequence("manuscript_ID"),
+        editor_ID : 9,
+        author_ID : 1,
+        title : "quis",
+        status : "under review",
+        status_time_stamp : "Thu Dec 22nd 2016 08:12:18",
+        RI_Code : 99
+    },
+    {
+        _id : getNextSequence("manuscript_ID"),
+        editor_ID : 4,
+        author_ID : 7,
+        title : "Cras",
+        status : "rejected",
+        status_time_stamp : "Wed Dec 21st 2016 03:12:52",
+        RI_Code : 64
+    },
+    {
+        _id : getNextSequence("manuscript_ID"),
+        editor_ID : 6,
+        author_ID : 2,
+        title : "viverra. Donec tempus,",
+        status : "in typesetting",
+        status_time_stamp : "Tue Aug 1st 2017 09:08:50",
+        RI_Code : 35
+    },
+    {
+        _id : getNextSequence("manuscript_ID"),
+        editor_ID : 4,
+        author_ID : 5,
+        title : "Proin velit.",
+        status : "published",
+        status_time_stamp : "Tue Sep 6th 2016 02:09:34",
+        RI_Code : 113
+    },
+    {
+        _id : getNextSequence("manuscript_ID"),
+        editor_ID : 8,
+        author_ID : 8,
+        title : "libero nec ligula",
+        status : "accepted",
+        status_time_stamp : "Mon Nov 20th 2017 08:11:58",
+        RI_Code : 32
+    },
+    {
+        _id : getNextSequence("manuscript_ID"),
+        editor_ID : 5,
+        author_ID : 3,
+        title : "ac ipsum. Phasellus",
+        status : "scheduled for publication",
+        status_time_stamp : "Sat Oct 21st 2017 04:10:56",
+        RI_Code : 34
+    },
+    {
+        _id : getNextSequence("manuscript_ID"),
+        editor_ID : 10,
+        author_ID : 4,
+        title : "erat. Sed",
+        status : "published",
+        status_time_stamp : "Tue Sep 27th 2016 10:09:12",
+        RI_Code : 43
+    },
+    {
+        _id : getNextSequence("manuscript_ID"),
+        editor_ID : 1,
+        author_ID : 3,
+        title : "dui. Fusce",
+        status : "in typesetting",
+        status_time_stamp : "Mon Jan 15th 2018 03:01:17",
+        RI_Code : 104
+    },
+    {
+        _id : getNextSequence("manuscript_ID"),
+        editor_ID : 6,
+        author_ID : 1,
+        title : "tortor. Nunc",
+        status : "under review",
+        status_time_stamp : "Sun Apr 29th 2018 02:04:47",
+        RI_Code : 4
+    },
+    {
+        _id : getNextSequence("manuscript_ID"),
+        editor_ID : 2,
+        author_ID : 10,
+        title : "at",
+        status : "in typesetting",
+        status_time_stamp : "Tue Mar 21st 2017 09:03:14",
+        RI_Code : 54
+    },
+    {
+        _id : getNextSequence("manuscript_ID"),
+        editor_ID : 1,
+        author_ID : 9,
+        title : "nec ante. Maecenas",
+        status : "scheduled for publication",
+        status_time_stamp : "Wed Sep 14th 2016 04:09:44",
+        RI_Code : 42
+    },
+    {
+        _id : getNextSequence("manuscript_ID"),
+        editor_ID : 6,
+        author_ID : 3,
+        title : "ac mattis",
+        status : "rejected",
+        status_time_stamp : "Tue May 2nd 2017 11:05:06",
+        RI_Code : 77
+    },
+    {
+        _id : getNextSequence("manuscript_ID"),
+        editor_ID : 10,
+        author_ID : 5,
+        title : "semper tellus",
+        status : "rejected",
+        status_time_stamp : "Wed Dec 20th 2017 11:12:02",
+        RI_Code : 46
+    },
+    {
+        _id : getNextSequence("manuscript_ID"),
+        editor_ID : 7,
+        author_ID : 3,
+        title : "metus urna",
+        status : "accepted",
+        status_time_stamp : "Tue Apr 3rd 2018 05:04:40",
+        RI_Code : 54
+    },
+    {
+        _id : getNextSequence("manuscript_ID"),
+        editor_ID : 1,
+        author_ID : 2,
+        title : "ullamcorper. Duis at",
+        status : "accepted",
+        status_time_stamp : "Tue Jul 4th 2017 12:07:50",
+        RI_Code : 32
+    },
+    {
+        _id : getNextSequence("manuscript_ID"),
+        editor_ID : 5,
+        author_ID : 5,
+        title : "feugiat.",
+        status : "accepted",
+        status_time_stamp : "Sun Apr 30th 2017 10:04:45",
+        RI_Code : 33
+    },
+    {
+        _id : getNextSequence("manuscript_ID"),
+        editor_ID : 3,
+        author_ID : 5,
+        title : "auctor,",
+        status : "under review",
+        status_time_stamp : "Wed Apr 11th 2018 06:04:59",
+        RI_Code : 68
+    },
+    {
+        _id : getNextSequence("manuscript_ID"),
+        editor_ID : 3,
+        author_ID : 3,
+        title : "nec",
+        status : "in typesetting",
+        status_time_stamp : "Fri Feb 16th 2018 04:02:44",
+        RI_Code : 6
+    },
+    {
+        _id : getNextSequence("manuscript_ID"),
+        editor_ID : 3,
+        author_ID : 3,
+        title : "auctor odio",
+        status : "under review",
+        status_time_stamp : "Tue Dec 5th 2017 02:12:18",
+        RI_Code : 48
+    },
+    {
+        _id : getNextSequence("manuscript_ID"),
+        editor_ID : 6,
+        author_ID : 9,
+        title : "aliquet",
+        status : "under review",
+        status_time_stamp : "Sun Nov 5th 2017 01:11:22",
+        RI_Code : 109
+    },
+    {
+        _id : getNextSequence("manuscript_ID"),
+        editor_ID: 7,
+        author_ID : 7,
+        title : "nunc",
+        status : "in typesetting",
+        status_time_stamp : "Wed Jun 14th 2017 08:06:22",
+        RI_Code : 108
+    }
+]);
