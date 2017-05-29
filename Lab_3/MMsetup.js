85a6018d608f2afa05b9e1599ac8e90d7f53f607
Julia Dressel <julia.dressel11@gmail.com>
2017-05-29 15:53:13 -0400

    setup

--- Lab_3/MMsetup.js
+++ Lab_3/MMsetup.js
@@ -156,7 +156,20 @@ function getNextSequence(name) {
    return ret.seq;
 }
 
+db.system.js.save({
+  _id:"getNextSequence",
+  value : function(name){
+    var ret = db.counters.findAndModify(
+           {
+             query: { _id: name },
+             update: { $inc: { seq: 1 } },
+             new: true
+           }
+    );
 
+    return ret.seq;
+   }
+});
 
 // insert RICodes with iterating _id
 db.RICodes.insertMany([
